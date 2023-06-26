64be57d23c2ef3bbfb70ffbf8dfbc7d94e917c53
williamjseim <33179905+williamjseim@users.noreply.github.com>
2023-06-26 19:31:41 +0200

    asd

---
+++ CrudMvvm/CrudMvvm/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DataAccessLibrary;
+
+namespace CrudMvvm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Binding dataContextBinding = new Binding();
+        ModelView viewModel = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataContextBinding.Source = viewModel.List;
+            dataContextBinding.Mode = BindingMode.TwoWay;
+            dataContextBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            viewModel.GetData();
+            ShitGrid.DataContext = dataContextBinding;
+            ShitGrid.ItemsSource = viewModel.List;
+            viewModel.List.CollectionChanged += test;
+        }
+
+        void test(object sender, EventArgs e)
+        {
+            ShitGrid.ItemsSource = viewModel.List;
+        }
+
+
+        private void EditData(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                shitModel data = (shitModel)((Button)e.Source).DataContext;
+                viewModel.EditData(data);
+            }
+            catch (Exception a) { Debug.WriteLine(a+" fail"); }
+        }
+
+        private void DeleteData(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                shitModel data = (shitModel)((Button)e.Source).DataContext;
+                viewModel.DeleteData(data);
+            }
+            catch (Exception a) { Debug.WriteLine(a + " fail"); }
+        }
+
+        private void CreateNewUser(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                viewModel.CreateNewUser();
+            }
+            catch (Exception a) { Debug.WriteLine(a + " fail"); }
+        }
+    }
+}
---
+++ CrudMvvm/CrudMvvm/ModelView.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLibrary;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace CrudMvvm
+{
+    public class ModelView : ObservableObject
+    {
+        SqlDataAccess sql = new();
+        TempDataGetter TempDataGetter;
+
+        private static ObservableCollection<shitModel> list = new();
+        public ObservableCollection<shitModel> List { get { return list; } set { list = value; OnPropertyChanged(); } }
+
+        public ModelView()
+        {
+            TempDataGetter = new(sql);
+        }
+
+        public async Task GetData()
+        {
+            List.Clear();
+            foreach (var item in await TempDataGetter.GetData())
+            {
+                list.Add(item);
+            }
+        }
+
+        public async void EditData(shitModel data)
+        {
+            await TempDataGetter.EditData(data);
+            Debug.Print(data.ToString());
+        }
+
+        public async void DeleteData(shitModel data)
+        {
+            List.Remove(data);
+            await TempDataGetter.DeleteData(data);
+        }
+        public async void CreateNewUser()
+        {
+            shitModel model = new shitModel { userName = "username", passWord = "password" };
+            await TempDataGetter.CreateNewUser(model);
+            GetData();
+        }
+    }
+}
